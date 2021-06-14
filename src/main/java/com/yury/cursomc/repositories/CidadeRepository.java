d24a6db559764269f7860cbfb17e5d645948d554
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-06-14 16:06:37 -0300

    Estado e Cidade

---
+++ src/main/java/com/yury/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.yury.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yury.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
---
+++ src/main/java/com/yury/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.yury.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yury.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
