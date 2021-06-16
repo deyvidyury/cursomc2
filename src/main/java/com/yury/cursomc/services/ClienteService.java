dfd8d68e1d727aa3b823ff4665bc9fdfe3d56ad0
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-06-16 09:40:25 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/yury/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.yury.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yury.cursomc.domain.Cliente;
+import com.yury.cursomc.repositories.ClienteRepository;
+import com.yury.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto nao encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
